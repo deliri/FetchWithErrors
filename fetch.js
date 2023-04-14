f6e24b10b0ca5cf10eccca27c8b73414280a54d6
Ase Deliri <deliri.ase@gmail.com>
2023-04-14 02:58:52 -0400

    updated to meet 2023 standards

--- fetch.js
+++ fetch.js
@@ -1,88 +1,70 @@
 const API_URL = "{{your_site_base_url}}";
 
-// Custom API error to throw
-function ApiError(message, data, status) {
-    let response = null;
-    let isObject = false;
+class ApiError extends Error {
+	constructor(message, data, status) {
+		super(message);
+		let response = null;
+		let isObject = false;
 
-    // We are trying to parse response
-    try {
-        response = JSON.parse(data);
-        isObject = true;
-    } catch (e) {
-        response = data;
-    }
+		try {
+			response = JSON.parse(data);
+			isObject = true;
+		} catch (e) {
+			response = data;
+		}
 
-    this.response = response;
-    this.message = message;
-    this.status = status;
-    this.toString = function () {
-        return `${ this.message }\nResponse:\n${ isObject ? JSON.stringify(this.response, null, 2) : this.response }`;
-    };
+		this.response = response;
+		this.status = status;
+		this.name = "ApiError";
+		this.message = `${message}\nResponse:\n${
+			isObject ? JSON.stringify(response, null, 2) : response
+		}`;
+	}
 }
 
-// API wrapper function
-const fetchResource = (path, userOptions = {}) => {
-    // Define default options
-    const defaultOptions = {};
-    // Define default headers
-    const defaultHeaders = {};
+async function fetchResource(path, userOptions = {}) {
+	const defaultOptions = {};
+	const defaultHeaders = {};
 
-    const options = {
-        // Merge options
-        ...defaultOptions,
-        ...userOptions,
-        // Merge headers
-        headers: {
-            ...defaultHeaders,
-            ...userOptions.headers,
-        },
-    };
+	const options = {
+		...defaultOptions,
+		...userOptions,
+		headers: {
+			...defaultHeaders,
+			...userOptions.headers,
+		},
+	};
 
-    // Build Url
-    const url = `${ API_URL }/${ path }`;
-    // Variable which will be used for storing response
-    let response = null;
+	const url = `${API_URL}/${path}`;
+	let response;
 
-    return fetch(url, options)
-        .then(responseObject => {
-            // Saving response for later use in lower scopes
-            response = responseObject;
+	try {
+		response = await fetch(url, options);
 
-            // HTTP unauthorized
-            if (response.status === 401) {
-                // Handle unauthorized requests
-                // Maybe redirect to login page?
-            }
+		if (response.status === 401) {
+			// Handle unauthorized requests, e.g., redirect to login page
+			return;
+		}
 
-            // Check for error HTTP error codes
-            if (response.status < 200 || response.status >= 300) {
-                // Get response as text
-                return response.text();
-            }
+		if (response.status < 200 || response.status >= 300) {
+			const errorText = await response.text();
+			throw new ApiError(
+				`Request failed with status ${response.status}.`,
+				errorText,
+				response.status,
+			);
+		}
 
-            // Get response as json
-            return response.json();
-        })
-        // "parsedResponse" will be either text or javascript object depending if
-        // "response.text()" or "response.json()" got called in the upper scope
-        .then(parsedResponse => {
-            // Check for HTTP error codes
-            if (response.status < 200 || response.status >= 300) {
-                // Throw error
-                throw parsedResponse;
-            }
-
-            // Request succeeded
-            return parsedResponse;
-        })
-        .catch(error => {
-            // Throw custom API error
-            // If response exists it means HTTP error occured
-            if (response) {
-                throw new ApiError(`Request failed with status ${ response.status }.`, error, response.status);
-            } else {
-                throw new ApiError(error.toString(), null, 'REQUEST_FAILED');
-            }
-        });
-};
+		return await response.json();
+	} catch (error) {
+		if (response) {
+			throw new ApiError(
+				`Request failed with status ${response.status}.`,
+				error,
+				response.status,
+			);
+		} else {
+			throw new ApiError(error.toString(), null, "REQUEST_FAILED");
+		}
+	}
+}
