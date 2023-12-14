43492d210e54d260ae9dec72c23dd7cc078321fa
Ase Deliri <deliri.ase@gmail.com>
2023-12-13 23:24:39 -0500

    modified the fetch capabilities

--- fetch.js
+++ fetch.js
@@ -1,70 +1,116 @@
+
+// Base URL for API requests
 const API_URL = "{{your_site_base_url}}";
 
-class ApiError extends Error {
-	constructor(message, data, status) {
-		super(message);
-		let response = null;
-		let isObject = false;
-
-		try {
-			response = JSON.parse(data);
-			isObject = true;
-		} catch (e) {
-			response = data;
-		}
-
-		this.response = response;
-		this.status = status;
-		this.name = "ApiError";
-		this.message = `${message}\nResponse:\n${
-			isObject ? JSON.stringify(response, null, 2) : response
-		}`;
-	}
+/**
+ * Parses a JSON string and returns an object. Returns the original data if parsing fails.
+ * @param {string} data - The JSON string to parse.
+ * @returns {object|string} - The parsed object or the original data.
+ */
+function tryParseJson(data) {
+  try {
+    return JSON.parse(data);
+  } catch {
+    return data;
+  }
 }
 
+/**
+ * Formats the API response for error messages.
+ * @param {object|string} response - The API response to format.
+ * @returns {string} - The formatted response.
+ */
+function formatResponse(response) {
+  return typeof response === 'object' ? JSON.stringify(response, null, 2) : response;
+}
+
+/**
+ * Constructs the full error message for API errors.
+ * @param {string} message - The error message.
+ * @param {any} data - The error data.
+ * @param {number} status - The HTTP status code.
+ * @returns {string} - The full error message.
+ */
+function constructErrorMessage(message, data, status) {
+  const response = tryParseJson(data);
+  return `${message}\nResponse:\n${formatResponse(response)}`;
+}
+
+/**
+ * Fetches a resource from the API.
+ * @param {string} path - The API endpoint path.
+ * @param {object} userOptions - The fetch options provided by the user.
+ * @returns {Promise<object>} - The fetched resource.
+ */
 async function fetchResource(path, userOptions = {}) {
-	const defaultOptions = {};
-	const defaultHeaders = {};
+  const options = prepareRequestOptions(userOptions);
+  const url = `${API_URL}/${path}`;
 
-	const options = {
-		...defaultOptions,
-		...userOptions,
-		headers: {
-			...defaultHeaders,
-			...userOptions.headers,
-		},
-	};
+  try {
+    const response = await fetch(url, options);
+    await handleResponse(response);
+    return response.json();
+  } catch (error) {
+    handleFetchError(error, response);
+  }
+}
 
-	const url = `${API_URL}/${path}`;
-	let response;
+/**
+ * Prepares the options for the fetch request.
+ * @param {object} userOptions - User-provided fetch options.
+ * @returns {object} - The prepared fetch options.
+ */
+function prepareRequestOptions(userOptions) {
+  const defaultOptions = {};
+  const defaultHeaders = {};
 
-	try {
-		response = await fetch(url, options);
+  return {
+    ...defaultOptions,
+    ...userOptions,
+    headers: {
+      ...defaultHeaders,
+      ...userOptions.headers,
+    },
+  };
+}
 
-		if (response.status === 401) {
-			// Handle unauthorized requests, e.g., redirect to login page
-			return;
-		}
+/**
+ * Handles the response from the fetch request.
+ * @param {Response} response - The response object from the fetch request.
+ * @returns {Promise<void>}
+ */
+async function handleResponse(response) {
+  if (response.status === 401) {
+    // Handle unauthorized requests, e.g., redirect to login
+    throw new Error('Unauthorized request');
+  }
 
-		if (response.status < 200 || response.status >= 300) {
-			const errorText = await response.text();
-			throw new ApiError(
-				`Request failed with status ${response.status}.`,
-				errorText,
-				response.status,
-			);
-		}
+  if (!response.ok) {
+    const errorText = await response.text();
+    const errorMessage = constructErrorMessage(
+      `Request failed with status ${response.status}.`,
+      errorText,
+      response.status
+    );
+    throw new Error(errorMessage);
+  }
+}
 
-		return await response.json();
-	} catch (error) {
-		if (response) {
-			throw new ApiError(
-				`Request failed with status ${response.status}.`,
-				error,
-				response.status,
-			);
-		} else {
-			throw new ApiError(error.toString(), null, "REQUEST_FAILED");
-		}
-	}
+/**
+ * Handles errors that occur during the fetch request.
+ * @param {Error} error - The error object.
+ * @param {Response} response - The response object from the fetch request.
+ * @throws {Error} - The constructed error with detailed message.
+ */
+function handleFetchError(error, response) {
+  if (response) {
+    const errorMessage = constructErrorMessage(
+      `Request failed with status ${response.status}.`,
+      error,
+      response.status
+    );
+    throw new Error(errorMessage);
+  } else {
+    throw new Error(error.toString());
+  }
 }
